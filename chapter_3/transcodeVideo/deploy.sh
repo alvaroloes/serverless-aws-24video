7df3fd14ba49520df737bba2ee08988609e8d4c5
Alvaro Lopez Espinosa <alvaro@theagilemonkeys.com>
2018-09-27 13:09:59 +0100

    Create deployment script

---
+++ chapter_3/transcodeVideo/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+FUNCTION_NAME="transcode-video"
+FUNCTION_ARN="arn:aws:lambda:us-east-1:785355572843:function:$FUNCTION_NAME"
+
+echo "-> Starting deployment of '$FUNCTION_NAME'"
+
+echo "-> Testing and vetting..."
+go test -vet all
+
+echo "-> Compiling..."
+GOOS=linux GOARCH=amd64 go build -o ./deploy/${FUNCTION_NAME} main.go
+
+echo "-> Zipping..."
+zip -j ./deploy/${FUNCTION_NAME}.zip ./deploy/${FUNCTION_NAME}
+
+echo "-> Deploying..."
+aws lambda update-function-code \
+  --function-name=${FUNCTION_ARN} \
+  --zip-file=fileb://deploy/${FUNCTION_NAME}.zip
+
+echo "-> Deployment finished."
+
+
