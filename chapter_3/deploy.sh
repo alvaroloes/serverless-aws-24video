13d638009bc1f7f9056909b38cf9bac843fa2f33
Alvaro Lopez Espinosa <alvaro@theagilemonkeys.com>
2018-10-03 12:19:34 +0100

    Make the "deploy.sh" script generic. It now can deploy any lambda

--- chapter_3/deploy.sh
+++ chapter_3/deploy.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-FUNCTION_NAME="transcode-video"
+if [ -z "$1" ]
+then
+    echo "No function name supplied"
+    exit 1
+fi
+
+FUNCTION_NAME="$1"
 FUNCTION_ARN="arn:aws:lambda:us-east-1:785355572843:function:$FUNCTION_NAME"
 
 echo "-> Starting deployment of '$FUNCTION_NAME'"
+
+cd ${FUNCTION_NAME}
 
 echo "-> Testing and vetting..."
 go test -vet all
@@ -20,5 +28,3 @@ aws lambda update-function-code \
   --zip-file=fileb://deploy/${FUNCTION_NAME}.zip
 
 echo "-> Deployment finished."
-
-
